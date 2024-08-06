ad57391b7e987cf6f4f60cc6759f4cc408f994a2
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-08-06 21:05:29 +0300

    add edit functionality

--- SimpleToDoListApp/index.js
+++ SimpleToDoListApp/index.js
@@ -1,25 +1,44 @@
-inputText = document.querySelector('input')
-mytasks = document.querySelector('ul')
-addButton = document.querySelector('button')
+inputText = document.querySelector('input');
+mytasks = document.querySelector("ul");
+addButton = document.querySelector('button');
 
-addButton.addEventListener('click' , () =>{
-    inputvalue = inputText.value
-    if (inputvalue !== '')  {
-        const task = document.createElement('li')
-        task.textContent = inputvalue
-        task.addEventListener('click', () => {
-            task.remove()
+addButton.addEventListener('click', () => {
+    const task_text = inputText.value;
+    if (task_text !== '') {
+         task = document.createElement('li');
+
+     task_content = document.createElement('p');
+        task_content.textContent = task_text;
+        task.append(task_content);
+           edit_btn = document.createElement('button');
+        edit_btn.textContent = "Edit";
+        task.appendChild(edit_btn);
+    delete_btn = document.createElement('button');
+        delete_btn.textContent = "Delete";
+        task.append(delete_btn);
+
+        edit_btn.addEventListener('click', () => {
+             edit_input = document.createElement('input');
+            edit_input.value = task_content.textContent;
+
+         save_btn = document.createElement('button');
+            save_btn.textContent = "Save";
+        task.replaceChild(edit_input, task_content);
+            task.replaceChild(save_btn, edit_btn);
+            save_btn.addEventListener('click', () => {
+                task_content.textContent = edit_input.value;
+                task.replaceChild(task_content, edit_input);
+                task.replaceChild(edit_btn, save_btn);
+            });
         });
-        mytasks.append(task)
-        inputText.value =''
-   
-        
+
+        delete_btn.addEventListener("click", () => {
+            task.remove();
+        });
+
+        inputText.value = "";
+        mytasks.append(task);
+    } else {
+        alert('empty input');
     }
-    else{
-        alert('enter the task first')
-    }
-    
-})
-
-
-
+});
